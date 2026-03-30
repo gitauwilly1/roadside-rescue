a473941a26f7d6d04887c1689137f8da884f853a
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-03-30 12:58:54 +0300

    add admin role to User model enum

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['client', 'garage'],
+    enum: ['client', 'garage', 'admin'],
     required: true
   },
   fullName: {
